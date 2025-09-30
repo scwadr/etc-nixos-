a9d441dbc9bfb15aa86e5cfe71205f70b688b6ca
Ken Shibata <ken.shibata@kiyuri.ca>
2025-09-30 19:46:59 -0400

    minamo uses an AMD CPU

--- minamo/configuration.nix
+++ minamo/configuration.nix
@@ -23,7 +23,7 @@
   ];
   boot.initrd.supportedFilesystems = [ "btrfs" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
