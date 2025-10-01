d26106586506521375ac8b8dede83e3920d7869d
Ken Shibata <ken.shibata@kiyuri.ca>
2025-10-01 00:51:04 -0400

    minamo: copy graphics settings from chikusa (same hardware)
    5022f3ae19ef4663d3655be3bd9510f86eee1fcb

--- minamo/configuration.nix
+++ minamo/configuration.nix
@@ -31,9 +31,13 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  hardware.graphics.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
   hardware.nvidia = {
     modesetting.enable = true;
+    open = false;
     nvidiaSettings = true;
   };
 
