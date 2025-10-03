fb34347154c85d4a534d8cb6f45f248af15efa9a
Ken Shibata <ken.shibata@kiyuri.ca>
2025-10-03 02:06:30 -0400

    nixfmt

--- niri/non-uwsm-binds.nix
+++ niri/non-uwsm-binds.nix
@@ -5,21 +5,23 @@
   ...
 }:
 {
-  config = lib.mkIf (config.kiyurica.desktop.niri.enable && !config.kiyurica.desktop.niri.enableUWSM) {
-    home-manager.users.kiyurica = {
-      imports = [
-        {
-          programs.niri.settings.binds = with config.lib.niri.actions; {
-            "Mod+Alt+L".action.spawn = [ "swaylock" ];
-            "Mod+Shift+Return".action.spawn = [ "firefox" ];
-            "Mod+Alt+Shift+Return".action.spawn = [ "chromium" ];
-            "Mod+Return".action.spawn = [ "footclient" ];
-            "Mod+Alt+Return".action.spawn = [
-              "${pkgs.rnote}/bin/rnote"
-            ];
-          };
-        }
-      ];
-    };
-  };
+  config =
+    lib.mkIf (config.kiyurica.desktop.niri.enable && !config.kiyurica.desktop.niri.enableUWSM)
+      {
+        home-manager.users.kiyurica = {
+          imports = [
+            {
+              programs.niri.settings.binds = with config.lib.niri.actions; {
+                "Mod+Alt+L".action.spawn = [ "swaylock" ];
+                "Mod+Shift+Return".action.spawn = [ "firefox" ];
+                "Mod+Alt+Shift+Return".action.spawn = [ "chromium" ];
+                "Mod+Return".action.spawn = [ "footclient" ];
+                "Mod+Alt+Return".action.spawn = [
+                  "${pkgs.rnote}/bin/rnote"
+                ];
+              };
+            }
+          ];
+        };
+      };
 }
