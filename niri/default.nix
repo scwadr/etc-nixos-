68b28f73013e94f6e1373c8a336727b16ae74b63
Ken Shibata <ken.shibata@kiyuri.ca>
2025-10-26 15:25:34 -0400

    idk

--- niri/default.nix
+++ niri/default.nix
@@ -267,24 +267,6 @@
                       "Mod+WheelScrollLeft".action = focus-column-left;
                       "Mod+Shift+WheelScrollRight".action = move-column-right;
                       "Mod+Shift+WheelScrollLeft".action = move-column-left;
-                      "Mod+1".action.focus-workspace = 1;
-                      "Mod+2".action.focus-workspace = 2;
-                      "Mod+3".action.focus-workspace = 3;
-                      "Mod+4".action.focus-workspace = 4;
-                      "Mod+5".action.focus-workspace = 5;
-                      "Mod+6".action.focus-workspace = 6;
-                      "Mod+7".action.focus-workspace = 7;
-                      "Mod+8".action.focus-workspace = 8;
-                      "Mod+9".action.focus-workspace = 9;
-                      "Mod+Shift+1".action.move-column-to-workspace = 1;
-                      "Mod+Shift+2".action.move-column-to-workspace = 2;
-                      "Mod+Shift+3".action.move-column-to-workspace = 3;
-                      "Mod+Shift+4".action.move-column-to-workspace = 4;
-                      "Mod+Shift+5".action.move-column-to-workspace = 5;
-                      "Mod+Shift+6".action.move-column-to-workspace = 6;
-                      "Mod+Shift+7".action.move-column-to-workspace = 7;
-                      "Mod+Shift+8".action.move-column-to-workspace = 8;
-                      "Mod+Shift+9".action.move-column-to-workspace = 9;
                       "Mod+Comma".action = consume-window-into-column;
                       "Mod+Period".action = expel-window-from-column;
                       "Mod+BracketLeft".action = consume-or-expel-window-left;
@@ -300,6 +282,7 @@
                       "Print".action = screenshot;
                       "Mod+Shift+E".action = quit;
                       "Mod+Shift+P".action = power-off-monitors;
+                      "Mod+Esc".action = toggle-keyboard-shortcuts-inhibit;
                     }
                     // (builtins.foldl' (a: b: a // b) { } (
                       builtins.map (i: {
