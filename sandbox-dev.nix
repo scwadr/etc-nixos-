fdd4940dc1f9f2964e1a89ae8c632287fceac4d5
Ken Shibata <ken.shibata@kiyuri.ca>
2025-12-16 22:35:00 +0900

    sandbox-dev: add script to enter nix develop shell with nixwrap

--- sandbox-dev.nix
+++ sandbox-dev.nix
@@ -8,7 +8,30 @@
 {
   options.kiyurica.sandbox-dev.enable = lib.mkEnableOption "sandboxed development environment based on bubblewrap";
 
-  config = lib.mkIf config.kiyurica.sandbox-dev.enable {
-    users.users.kiyurica.packages = [ nixwrap.packages.${pkgs.stdenv.hostPlatform.system}.wrap ];
-  };
+  config =
+    let
+      wrap = nixwrap.packages.${pkgs.stdenv.hostPlatform.system}.wrap;
+      sandbox-dev-command = pkgs.writeShellScriptBin "sandbox-dev-command" ''
+        KIYURICA_IN_SANDBOX_DEV=yes nix develop --command wrap -e KIYURICA_IN_SANDBOX_DEV -r "$HOME/.config/fish" "$@" "$SHELL"
+      '';
+    in
+    lib.mkIf config.kiyurica.sandbox-dev.enable {
+      users.users.kiyurica.packages = [
+        wrap
+        sandbox-dev-command
+      ];
+
+      kiyurica.home-manager.enable = true;
+      home-manager.users.kiyurica.programs.fish.interactiveShellInit = ''
+        functions -c fish_prompt _original_fish_prompt
+        function fish_prompt
+          if test -n "$KIYURICA_IN_SANDBOX_DEV"
+            set_color -o red
+            echo -n '[SANDBOX] '
+            set_color normal
+          end
+          _original_fish_prompt
+        end
+      '';
+    };
 }
