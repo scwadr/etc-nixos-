5a1335de5ea9e13d350b31908533215b2ab2da5a
Ken Shibata <ken.shibata@kiyuri.ca>
2025-12-23 14:39:07 +0900

    inaho: add webdav directory for convind4

--- inaho/webdav.nix
+++ inaho/webdav.nix
@@ -34,6 +34,22 @@
           auth_basic_user_file ${config.age.secrets.webdav-htpasswd.path};
         '';
       };
+
+      locations."/convind4" = {
+        root = "/var/lib/webdav/convind4";
+        extraConfig = ''
+          client_body_temp_path /var/lib/webdav/tmp;
+          dav_methods PUT DELETE MKCOL COPY MOVE;
+          dav_ext_methods PROPFIND OPTIONS;
+          dav_access user:rw group:rw all:r;
+
+          client_max_body_size 0;
+          create_full_put_path on;
+
+          auth_basic "WebDAV";
+          auth_basic_user_file ${config.age.secrets.webdav-htpasswd.path};
+        '';
+      };
     };
   };
   systemd.services.nginx.serviceConfig.StateDirectory = "webdav";
