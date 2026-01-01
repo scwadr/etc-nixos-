4417b7f43e4255873280ed37ed14d23534f9ba17
Ken Shibata <ken.shibata@kiyuri.ca>
2026-01-01 23:54:48 +0900

    mitsu8: update NHK link

--- mitsu8/configuration.nix
+++ mitsu8/configuration.nix
@@ -102,7 +102,7 @@
           command = "${pkgs.chromium}/bin/chromium '--proxy-server=socks5://10.42.0.1:1080' --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36' https://tver.jp";
         }
         {
-          command = "${pkgs.microsoft-edge}/bin/microsoft-edge '--proxy-server=socks5://10.42.0.1:1080' https://plus.nhk.jp";
+          command = "${pkgs.microsoft-edge}/bin/microsoft-edge '--proxy-server=socks5://10.42.0.1:1080' https://www.web.nhk";
         }
       ];
 
