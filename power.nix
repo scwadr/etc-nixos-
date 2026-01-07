cfc386b6cff55ff9c2436af7b09f82f097ed1129
Ken Shibata <ken.shibata@kiyuri.ca>
2026-01-07 22:04:38 +0900

    power: increase charge start threshold
    for longer runtime (e.g., classes)

--- power.nix
+++ power.nix
@@ -10,7 +10,8 @@
     # preserve lifetime. Run `tlp fullcharge` to temporarily force
     # full charge.
     # https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
-    START_CHARGE_THRESH_BAT0 = 60;
+    # https://support.lenovo.com/jp/ja/solutions/ht078208-how-can-i-increase-battery-life-thinkpad-and-lenovo-vbke-series-notebooks
+    START_CHARGE_THRESH_BAT0 = 85;
     STOP_CHARGE_THRESH_BAT0 = 90;
 
     # 100 being the maximum, limit the speed of my CPU to reduce
