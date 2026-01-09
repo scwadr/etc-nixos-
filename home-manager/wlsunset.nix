c2a8a1e1b3f7a6d9c832381dc1afa4bfb5cf97d3
Ken Shibata <ken.shibata@kiyuri.ca>
2026-01-08 23:07:49 -0500

    wlsunset: to atlanta

--- home-manager/wlsunset.nix
+++ home-manager/wlsunset.nix
@@ -2,12 +2,12 @@
 {
   services.wlsunset = {
     enable = true;
-    latitude = lib.mkDefault "35.67"; # Tokyo
-    longitude = lib.mkDefault "139.65";
+    # latitude = lib.mkDefault "35.67"; # Tokyo
+    # longitude = lib.mkDefault "139.65";
     # latitude = lib.mkDefault "43.65"; # Toronto
     # longitude = lib.mkDefault "-79.38";
-    # latitude = lib.mkDefault "33.7501"; # Atlanta
-    # longitude = lib.mkDefault "-84.3885";
+    latitude = lib.mkDefault "33.7501"; # Atlanta
+    longitude = lib.mkDefault "-84.3885";
     temperature = {
       day = lib.mkDefault 6500;
       night = lib.mkDefault 2000;
