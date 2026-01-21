c0643e81c217998e88b9675c958b30af17fcf114
Ken Shibata <ken.shibata@kiyuri.ca>
2026-01-20 20:04:05 -0500

    powertop fix

--- powertop-tune.nix
+++ powertop-tune.nix
@@ -12,15 +12,43 @@
   };
 
   config = lib.mkIf config.powertop-tune.enable {
-    systemd.services.powertop-tune = {
-      description = "run powertop tunings";
-      serviceConfig.ExecStart = "${pkgs.bash}/bin/sh ${config.powertop-tune.path}";
-      serviceConfig.ExecStartPost = ''${pkgs.bash}/bin/bash -c 'for f in $(find /sys/bus/usb/drivers/usbhid -regex ".*/[0-9:.-]+" -printf "%f\\n" | cut -d ":" -f 1 | sort -u); do echo on >| "/sys/bus/usb/devices/$f/power/control"; done' '';
-      serviceConfig.Type = "oneshot";
-      wantedBy = [
-        "multi-user.target"
-        "sleep.target"
-      ];
-    };
+    systemd.services.powertop-tune =
+      let
+        usbhidPowerControl = pkgs.writeShellScript "powertop-usbhid-power-control" ''
+          set -u
+
+          usb_devs=$(
+            for hid in /sys/bus/usb/drivers/usbhid/*; do
+              [ -e "$hid" ] || continue
+              target=$(readlink -f "$hid" 2>/dev/null || true)
+              [ -n "$target" ] || continue
+
+              p="$target"
+              while [ "$p" != "/" ] && [ "$p" != "/sys" ]; do
+                b=$(basename "$p")
+                if [ -e "/sys/bus/usb/devices/$b/power/control" ]; then
+                  echo "$b"
+                  break
+                fi
+                p=$(dirname "$p")
+              done
+            done | sort -u
+          )
+
+          for dev in $usb_devs; do
+            echo on >| "/sys/bus/usb/devices/$dev/power/control" 2>/dev/null || true
+          done
+        '';
+      in
+      {
+        description = "run powertop tunings";
+        serviceConfig.ExecStart = "${pkgs.bash}/bin/sh ${config.powertop-tune.path}";
+        serviceConfig.ExecStartPost = "${usbhidPowerControl}";
+        serviceConfig.Type = "oneshot";
+        wantedBy = [
+          "multi-user.target"
+          "sleep.target"
+        ];
+      };
   };
 }
